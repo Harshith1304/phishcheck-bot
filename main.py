44cbf36f045f7fc45f94372586ff901d4dcd4e05
Harshith1304 <gajula.harshith13@gmail.com>
2025-05-07 00:10:59 +0530

    Update main.py

--- main.py
+++ main.py
@@ -124,7 +124,7 @@ async def telegram_webhook():
 @app.route("/")
 def index():
     return "PhishCheck Bot is up."
-@app.route("/uptime", methods=['GET','POST'])
+@app.route("/uptime", methods=['GET','HEAD'])
 def uptime():
     return "OK", 200
 
